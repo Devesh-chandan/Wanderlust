0f260b48342b4741792a7b932733c8f18777976d
Devesh-chandan <crea8x1@gmail.com>
2026-01-22 18:19:04 +0530

    changes in mongo store

--- app.js
+++ app.js
@@ -18,29 +18,43 @@ const listingsRouter=require("./routes/listing.js");
 const reviewsRouter=require("./routes/reviews.js");
 const userRouter = require("./routes/user.js");
 const session=require("express-session");
-const MongoStore = require("connect-mongo");
+
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
 
 // const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl=process.env.ATLASDB_URL;
+
+// Use the Atlas URL from .env, or fallback to local for development safety
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
 
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 
-main()
-.then(()=>{
-    console.log("connected to DB"); 
-})
-.catch(
-    err => console.log(err)
-);
+// main()
+// .then(()=>{
+//     console.log("connected to DB"); 
+// })
+// .catch(
+//     err => console.log(err)
+// );
 
-async function main() {
-  await mongoose.connect(dbUrl);
-};
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// };
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -60,42 +74,30 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 
+
+
+
+
+
+
 // const store = MongoStore.create({
 //     mongoUrl: dbUrl,
 //     crypto: {
-//         secret: process.env.SESSION_SECRET,
+//         secret:"secret",
 //     },
 //     touchAfter: 24 * 3600,
-// }).default;
+// });
 
-
-
-// store.on("error",(err)=>{
-//     console.log("error in mongo stroe",err);
-// })
-
-
-
-
-
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret:"secret",
-    },
-    touchAfter: 24 * 3600,
-});
-
-store.on("error", (err) => {
-    console.log("ERROR IN MONGO SESSION STORE", err);
-});
+// store.on("error", (err) => {
+//     console.log("ERROR IN MONGO SESSION STORE", err);
+// });
 
 
 
 
 
 const sessionOptions={
-    store,
+   
     secret:"secret",
     resave:false,
     saveUninitialized:true,
